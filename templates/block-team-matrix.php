5025b26412653d055a3effa27ef7911df5fef27e
Andrés Felipe Torres <andres.torres@polymathventures.co>
2017-02-20 17:24:39 -0500

    Add options for Polymath branches to team
    - Polymath goes to México!!!

--- templates/block-team-matrix.php
+++ templates/block-team-matrix.php
@@ -1,112 +1,118 @@
 <?php
-
 $args = array(
-	'posts_per_page'   => -1,
-	'orderby'          => 'meta_value_num title',
-	'meta_key'		   => 'order',
-	'post_type'		   => 'team_members',
-	'order'            => 'ASC',
-	'post_status'      => 'publish',
+	"posts_per_page" => -1,
+	"orderby"        => "meta_value_num title",
+	"meta_key"		   => "order",
+	"post_type"		   => "team_members",
+	"order"          => "ASC",
+	"post_status"    => "publish",
 );
 
-if($params['role']) {
-	$args['tax_query'] = array(
-							array(
-							'taxonomy' => $params['role']->taxonomy,
-							'field' => 'id',
-							'terms' => $params['role']->term_id
-							)
-						);
+if( $params["role"] ) {
+	$args["tax_query"] = array(
+		array(
+			"taxonomy" => $params["role"]->taxonomy,
+			"field"    => "id",
+			"terms"    => $params["role"]->term_id
+		)
+	);
 }
 
-if ($params['category']) {
-	$args['meta_query'] = 	array(
-								array(
-								'key' => $params['category']->post_type,
-								'value' => '"' . $params['category']->ID . '"',
-								'compare' => 'LIKE'
-								)
-							);
+if( $params["category"] ) {
+	$args["meta_query"] = array(
+		array(
+			"key"     => $params["category"]->post_type,
+			"value"   => "'" . $params["category"]->ID . "'",
+			"compare" => "LIKE"
+		)
+	);
 }
 
 $people = new WP_query($args);
-
 ?>
 
 <?php
+$countries = []; $classes = [];
+foreach( $people->posts as $team_member ) {
+	foreach( wp_get_post_tags($team_member->ID) as $tag ) {
+		if( !in_array($tag->name, $countries) ) {
+			$countries[] = $tag->name;
+			$classes[]   = strtolower($tag->name);
+		}
+	}
+}
+$items = array_combine($classes, $countries);
+$items["button_text"] = "Countries";
+include(locate_template("templates/element-matrix-filter.php"));
+?>
 
+<span class="custom-button">&middot;</span>
+
+<?php
 $args = array(
-	'posts_per_page'   => -1,
-	'orderby'          => 'post_title',
-	'order'            => 'ASC',
-	'post_type'        => 'ventures',
-	'post_status'      => array('publish', 'pending'),
+	"posts_per_page" => -1,
+	"orderby"        => "post_title",
+	"order"          => "ASC",
+	"post_type"      => "ventures",
+	"post_status"    => array("publish", "pending"),
 );
 
 $items = get_posts( $args );
-$items[0]->button_text = 'Ventures';
+$items[0]->button_text = "Ventures";
 ?>
 
-<?php if(!$params['category']): ?>
-	<?php include(locate_template('templates/element-matrix-filter.php')); ?>
+<?php if( !$params["category"] ): ?>
+	<?php include(locate_template("templates/element-matrix-filter.php")); ?>
 <?php endif; ?>
 
 
-<?php if(!$params['category'] && !$params['role']): ?>
+<?php if( !$params["category"] && !$params["role"] ): ?>
 	<span class="custom-button">&middot;</span>
 <?php endif; ?>
 
 <?php
-
-$items = get_terms( array(
-	'taxonomy' => 'job_role',
-) );
-$items[0]->button_text = 'Role';
-$excluded = ['management'];
+$items = get_terms(array(
+	"taxonomy" => "job_role",
+));
+$items[0]->button_text = "Role";
+$excluded = ["management"];
 ?>
 
-<?php if(!$params['role']): ?>
-	<?php include(locate_template('templates/element-matrix-filter.php')); ?>
+<?php if( !$params["role"] ): ?>
+	<?php include(locate_template("templates/element-matrix-filter.php")); ?>
 <?php endif; ?>
 
 <span class="custom-button">&middot;</span>
 <button type="button" class="btn custom-button filter" data-filter="all">
-    <span>Clear filters</span>
+	<span>Clear filters</span>
 </button>
 
-<br/>
-<br/>
-<br/>
+<br><br><br>
 
 <?php if( $people->have_posts() ): ?>
+	<div class="mixitup-container">
+		<?php while( $people->have_posts() ) : $people->the_post(); ?>
+			<?php if(! get_field("order") ) continue; ?>
+			<?php $person    = formatPersonInfo(get_post()); ?>
+			<?php $countries = implode(" ", array_map(function($tag) {return strtolower($tag->name);}, wp_get_post_tags(get_the_ID())) ?: []); ?>
+			<?php $country_title = "<span class='small' style='opacity:0.6; position:relative; top:-5px;'>".strtoupper($countries)."</span>"; ?>
+			<?php $ventures  = implode(" ", get_field( "ventures" ) ?: []); ?>
+			<?php $seeds     = implode(" ", get_field( "seeds" ) ?: []); ?>
+			<?php $roles     = implode(" ", array_map(function($role) {return $role->slug;}, get_the_terms(get_the_ID(), "job_role") ?: [])); ?>
 
-<div class="mixitup-container">
-
-	<?php while( $people->have_posts() ) : $people->the_post(); ?>
-
-		<?php if(!get_field('order')) continue; ?>
-
-		<?php $person = formatPersonInfo(get_post()); ?>
-		<?php $ventures = implode(' ', get_field( "ventures" ) ?: []); ?>
-		<?php $seeds = implode(' ', get_field( "seeds" ) ?: []); ?>
-		<?php $roles = implode(' ', array_map(function($role) {return $role->slug;}, get_the_terms(get_the_ID(), 'job_role') ?: [])); ?>
-
-		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mix team-member responsive-bg <?php echo $ventures; ?> <?php echo $seeds; ?> <?php echo $roles; ?>"
-             data-bg-json='<?php echo json_encode(format_attachment_sizes_array(get_post_thumbnail_id())); ?>'>
-			 <span id="person-<?php the_ID(); ?>" class="anchor"></span>
-				 <article class="team-member-info-overlay content-padding-wrapper show-person-modal"
-						  data-title="<?php echo htmlspecialchars($person['title']); ?>"
-						  data-description="<?php echo htmlspecialchars($person['full_description']); ?>"
-						  data-picture="<?php echo $person['image']['sizes']['original']; ?>">
-				 <div class="content-padding">
-						 <h3><?php echo $person['title']; ?></h3>
-			  		</div>
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mix team-member responsive-bg <?php echo $countries; ?> <?php echo $ventures; ?> <?php echo $seeds; ?> <?php echo $roles; ?>"
+				data-bg-json='<?php echo json_encode(format_attachment_sizes_array(get_post_thumbnail_id())); ?>'>
+				<span id="person-<?php the_ID(); ?>" class="anchor"></span>
+				<article class="team-member-info-overlay content-padding-wrapper show-person-modal"
+					data-title="<?php echo htmlspecialchars($person['title']); ?><?php echo strpos($countries, ' ')===false ? '<br>'.htmlspecialchars($country_title) : ''; ?>"
+					data-description="<?php echo htmlspecialchars($person['full_description']); ?>"
+					data-picture="<?php echo $person['image']['sizes']['original']; ?>">
+					<div class="content-padding">
+						<h3><?php echo $person["title"]; ?></h3>
+					</div>
 				</article>
-		</div>
-
-	<?php endwhile; ?>
-
-</div>
-
+			</div>
+		<?php endwhile; ?>
+	</div>
 <?php endif; ?>
 <?php wp_reset_query(); ?>
--- templates/element-matrix-filter.php
+++ templates/element-matrix-filter.php
@@ -1,33 +1,31 @@
 <?php
+if( is_object(reset($items)) ) {
+	$button = $items[0]->button_text;
 
-if(is_object(reset($items))) {
-    $button = $items[0]->button_text;
+	$labels = array();
 
-    $labels = array();
+	foreach( $items as $item ) {
+		$key = isset($item->ID) ? $item->ID : $item->slug;
+		$val = isset($item->post_title) ? $item->post_title : $item->name;
 
-    foreach($items as $item) {
-        $key = isset($item->ID) ? $item->ID : $item->slug;
-        $val = isset($item->post_title) ? $item->post_title : $item->name;
-
-        $labels[$key] = $val;
-    }
+		$labels[$key] = $val;
+	}
 } else {
-
-    $labels = $items;
-    $button = array_pop($labels);
-
+	$labels = $items;
+	$button = array_pop($labels);
 }
-
 ?>
 
 <div class="btn-group">
-    <button type="button" class="btn custom-button" data-label="<?php echo $button; ?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span><?php echo $button; ?> <i class="caret"></i></span>
-    </button>
-    <ul class="dropdown-menu bullet pull-center custom-dropdown">
-        <?php foreach($labels as $key => $val): ?>
-            <?php if(isset($excluded) && in_array($key, $excluded)) continue; ?>
-            <li><a href="#" class="filter" data-filter=".<?php echo $key; ?>"><?php echo $val; ?></a></li>
-        <?php endforeach; ?>
-    </ul>
+	<button type="button" class="btn custom-button" data-label="<?php echo $button; ?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		<span><?php echo $button; ?> <i class="caret"></i></span>
+	</button>
+	<ul class="dropdown-menu bullet pull-center custom-dropdown">
+		<?php foreach( $labels as $key=>$val ): ?>
+			<?php if( isset($excluded) && in_array($key, $excluded) ) continue; ?>
+			<li>
+				<a href="#" class="filter" data-filter=".<?php echo $key; ?>"><?php echo $val; ?></a>
+			</li>
+		<?php endforeach; ?>
+	</ul>
 </div>
