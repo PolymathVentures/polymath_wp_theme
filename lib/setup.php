d48e53a90af4f815dc80c6fde73ef3e48cf11bfb
Andrés Felipe Torres <andres@polymathv.com>
2018-02-06 12:08:50 -0500

    Add roots/soil theme support

--- lib/setup.php
+++ lib/setup.php
@@ -11,9 +11,11 @@ function setup() {
   // Enable features from Soil when plugin is activated
   // https://roots.io/plugins/soil/
   add_theme_support('soil-clean-up');
+  add_theme_support('soil-google-analytics', 'UA-XXXXX-Y');
+  add_theme_support('soil-jquery-cdn');
+  add_theme_support('soil-js-to-footer');
   add_theme_support('soil-nav-walker');
   add_theme_support('soil-nice-search');
-  add_theme_support('soil-jquery-cdn');
   add_theme_support('soil-relative-urls');
 
   // Make theme available for translation
